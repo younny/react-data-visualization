1fee35a18c30a4f6e0be18e56503c269b58085a6
Dooyoung Gi <dooyounny@gmail.com>
2018-12-02 11:06:39 +0900

    Change filter text to korean.

--- src/route/data-visualization.js
+++ src/route/data-visualization.js
@@ -5,6 +5,7 @@ import Dropdown from 'react-bootstrap/lib/Dropdown'
 import DropdownButton from 'react-bootstrap/lib/DropdownButton'
 import ButtonToolbar from 'react-bootstrap/lib/ButtonToolbar'
 import BasicMap from '../component/BasicMap'
+import i18n from '../i18n'
 
 class DataVisualizationScreen extends React.Component {
 	constructor(props) {
@@ -19,27 +20,27 @@ class DataVisualizationScreen extends React.Component {
 						<ButtonToolbar style={{ marginTop: "5%" }}>
 							<DropdownButton
 							 	style={{ marginRight: 5 }}
-								title="Select Data"
+								title={i18n.t("data_filter.filter1")}
 								variant="info">
-							    <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
-							    <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
-							    <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+							    <Dropdown.Item href="#/action-1">데이터 1</Dropdown.Item>
+							    <Dropdown.Item href="#/action-2">데이터 2</Dropdown.Item>
+							    <Dropdown.Item href="#/action-3">데이터 3</Dropdown.Item>
 							</DropdownButton>
 							<DropdownButton
 								style={{ marginRight: 5 }}
-								title="Filter"
+								title={i18n.t("data_filter.filter2")}
 								variant="info">
-							    <Dropdown.Item href="#/action-1">By Distance</Dropdown.Item>
-							    <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
-							    <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+							    <Dropdown.Item href="#/action-1">거리</Dropdown.Item>
+							    <Dropdown.Item href="#/action-2">시간</Dropdown.Item>
+							    <Dropdown.Item href="#/action-3">번호</Dropdown.Item>
 							</DropdownButton>
 							<DropdownButton
 								style={{ marginRight: 5 }}
-								title="Other"
+								title={i18n.t("data_filter.filter3")}
 								variant="info">
-							    <Dropdown.Item href="#/action-1">By Distance</Dropdown.Item>
-							    <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
-							    <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+							    <Dropdown.Item href="#/action-1">기타1</Dropdown.Item>
+							    <Dropdown.Item href="#/action-2">기타2</Dropdown.Item>
+							    <Dropdown.Item href="#/action-3">기타3</Dropdown.Item>
 							</DropdownButton>
 						</ButtonToolbar>
 					<div style={{ marginTop: 40 }}>
